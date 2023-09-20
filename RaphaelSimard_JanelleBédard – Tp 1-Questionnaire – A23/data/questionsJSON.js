b29cfeb30c5c9a9906506eaa16c569f7764222c4
JanelleB2 <2264502@cegeplimoilou.ca>
2023-09-19 20:33:26 -0400

    1st question
    asdfhggdszcvdfbdgndfnxg hmzfgmnzfgjmk

--- RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/data/questionsJSON.js
+++ RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/data/questionsJSON.js
@@ -5,3 +5,18 @@
 //Mettre vos questions au format JSON ici. Un seul objet principal pour toutes les questions
 
 //Faites quelque chose de simple comme on a vu ensemble.
+//tableau associatif == MAP
+
+[
+    {
+        "question": "Quelle méthode n'existe pas dans le DOM ?",
+        "optionA": "document.getElementsByBigMac",
+        "optionB": "document.getElementsByClassName",
+        "optionC": "document.getElementById",
+        "optionD": "document.getElementsByTagName",
+
+        "reponse": "document.getElementByBigMac"
+    }
+
+    ]
+
