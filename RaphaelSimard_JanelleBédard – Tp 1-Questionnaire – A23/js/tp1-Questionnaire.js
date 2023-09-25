cabc915b9c10c7bafe4d54bbec6f4e82329dfc42
JanelleB2 <2264502@cegeplimoilou.ca>
2023-09-25 19:53:14 -0400

    amélioration du css
    inshallah salam aleykoum kebab sharwama masha'allah allah <3

--- RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
+++ RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
@@ -11,15 +11,15 @@ rectangle.id = "rectangle";
 // ici on fait la magnifique création du titre
 const titre = document.createElement("div");
 titre.id = "titre";
-titre.textContent = " السَّلَامُ عَلَيْكُمْ وَ رَحْمَةُ اللهِ وَ بَرَكاتُهُ Isha'allah, bienvenue sur notre quiz  ما شاء الله";
+titre.textContent = "Salam, bienvenue sur le quiz JavaScript de Janelle et Raphael";
 
 // ici on fait la création des paragraphes
 
 const paragraphe1 = document.createElement("p1");
 paragraphe1.textContent = "JEU QUESTIONNAIRE :o";
 const paragraphe2 = document.createElement("p2");
-paragraphe2.textContent = "Je vous invite à participer à un petit jeu-questionnaire qui comporte 5 questions choisies au hasard dans un ensemble de questions. " +
-    "Pour chaque question, vous devez choisir une réponse parmi les 4 choix proposés. En souhaitant que vous ayez autant de plaisir que nous qui fait ce script 0_0" + "'";
+paragraphe2.textContent = "Je vous invite à participer à un petit jeu questionnaire qui comporte 5 questions choisies au hasard dans un ensemble de questions. " +
+    "Pour chaque question, vous devez choisir une réponse parmi les 4 choix proposés. À la fin du questionnaire, vous aurez votre résultat. ";
 
 
 // ici on fait la création du bouton
@@ -31,11 +31,15 @@ bouton.addEventListener("click", function () {
     viderLintérieurDuRectangle();
 });
 
+/**
+ * Fonction qui vide le rectangle après le click du bouton
+ */
 function viderLintérieurDuRectangle() {
-    for (let i = 0; i < 4; i++) {
-        rectangle.removeChild(rectangle.childNodes[0]);
+    while (rectangle.firstChild) {
+        rectangle.removeChild(rectangle.firstChild);
     }
 }
+
 // on fait les appends, ca ressemble a ce que le prof a fait
 rectangle.appendChild(titre);
 rectangle.appendChild(paragraphe1);
