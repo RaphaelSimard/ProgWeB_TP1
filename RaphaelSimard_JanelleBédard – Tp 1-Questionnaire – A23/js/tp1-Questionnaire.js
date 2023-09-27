720e23f1bd368920586685ed135535adb5b5738d
RaphaelSimard <raphaelsimard21@gmail.com>
2023-09-26 20:25:54 -0400

    Modification de la structure du tp1-questionnaire...
    merci pour les stats git

--- RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
+++ RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
@@ -3,51 +3,32 @@
 //Mettre votre code JS ici
 
 const rectangleDesDonnées = document.getElementById("zoneDeDonnees");
-
-
 const rectangle = document.createElement("div");
 rectangle.id = "rectangle";
 
-// ici on fait la magnifique création du titre
-const titre = document.createElement("div");
-titre.id = "titre";
-titre.textContent = "Salam maleykoum, bienvenue sur le quiz JavaScript de Janelle et Raphael";
-
-// ici on fait la création des paragraphes
-
-const paragraphe1 = document.createElement("p1");
-paragraphe1.textContent = "JEU QUESTIONNAIRE :o";
-const paragraphe2 = document.createElement("p2");
-paragraphe2.textContent = "Je vous invite à participer à un petit jeu questionnaire qui comporte 5 questions choisies au hasard dans un ensemble de questions. " +
-    "Pour chaque question, vous devez choisir une réponse parmi les 4 choix proposés. À la fin du questionnaire, vous aurez votre résultat. ";
-
-
-// ici on fait la création du bouton
-const bouton = document.createElement("bouton");
-bouton.id = "bouton";
-bouton.textContent = "Commencer le quiz";
-bouton.addEventListener("click", function () {
-    alert("Vous avez cliqué sur le bouton")
-    viderLintérieurDuRectangle();
-});
-
-/**
- * Fonction qui vide le rectangle après le click du bouton
- */
-function viderLintérieurDuRectangle() {
-    while (rectangle.firstChild) {
-        rectangle.removeChild(rectangle.firstChild);
-    }
+function créerBouton() {
+    let bouton = creerBaliseX("button", "bouton", "Commencer le quiz", "");
+    bouton.addEventListener("click", function () {
+        alert("Vous avez cliqué sur le bouton")
+    });
+    return bouton;
+}
+function constuireInterfaceQuestion() {
+    rectangle.appendChild(creerBaliseX("h1", "titre", "Bienvenue sur le quiz JavaScript de Janelle et Raphael"));
+    rectangle.appendChild(creerBaliseX("p", "p1", "JEU QUESTIONNAIRE :o"));
+    rectangle.appendChild(creerBaliseX("p", "p2", "Je vous invite à participer à un petit jeu questionnaire qui comporte 5 questions choisies au hasard dans un ensemble de questions. "));
+    rectangle.appendChild(créerBouton());
 }
 
-// on fait les appends, ca ressemble a ce que le prof a fait
-rectangle.appendChild(titre);
-rectangle.appendChild(paragraphe1);
-rectangle.appendChild(paragraphe2);
-rectangle.appendChild(bouton);
-
-// fait le append du rectangle dans le rectangle des données
+// Call the function to construct the interface
+constuireInterfaceQuestion();
 rectangleDesDonnées.appendChild(rectangle);
+
+
+
+
+
+
 
 
 
