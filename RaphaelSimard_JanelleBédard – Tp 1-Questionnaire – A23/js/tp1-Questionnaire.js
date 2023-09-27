39ac25c8564b0b9fe1931ff0f5afc162cf118c89
Janelle Bédard <2264502@cegeplimoilou.ca>
2023-09-27 12:29:49 -0400

    J'AI ENCORE ENLEVER TES ACCENTS RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
    WHAT THE FUCK IS A FRENCH CLAVIER RAAAAAAH AMERICAAAAAAAAA POW POW POW BOOM BOOM

--- RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
+++ RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
@@ -2,11 +2,11 @@
 
 //Mettre votre code JS ici
 
-const rectangleDesDonnées = document.getElementById("zoneDeDonnees");
+const rectangleDesDonnees = document.getElementById("zoneDeDonnees");
 const rectangle = document.createElement("div");
 rectangle.id = "rectangle";
 
-function créerBouton() {
+function creerBouton() {
     let bouton = creerBaliseX("button", "bouton", "Commencer le quiz", "");
     bouton.addEventListener("click", function () {
         alert("Vous avez cliqué sur le bouton")
@@ -24,7 +24,7 @@ function constuireInterfaceQuestion() {
     rectangle.appendChild(creerBaliseX("h1", "titre", "Bienvenue sur le quiz JavaScript de Janelle et Raphael"));
     rectangle.appendChild(creerBaliseX("p", "p1", "JEU QUESTIONNAIRE :o"));
     rectangle.appendChild(creerBaliseX("p", "p2", "Je vous invite à participer à un petit jeu questionnaire qui comporte 5 questions choisies au hasard dans un ensemble de questions. "));
-    rectangle.appendChild(créerBouton());
+    rectangle.appendChild(creerBouton());
 
 }
 
@@ -57,7 +57,7 @@ function creationTableauQuestions(){
 
 
 
-constuireInterfaceQuestion(rectangleDesDonnées.appendChild(rectangle));
+constuireInterfaceQuestion(rectangleDesDonnees.appendChild(rectangle));
 creationTableauQuestions();
 alert("allo")
 
