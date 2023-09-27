3c5ddab849735137ae58ed53be325092db3802f0
RaphaelSimard <raphaelsimard21@gmail.com>
2023-09-26 20:34:25 -0400

    jsp si ca va marcher mais bon
    j'aime manger des barres tendres aux granolas pendant mes cours de BDII :)

--- RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
+++ RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
@@ -20,56 +20,8 @@ function constuireInterfaceQuestion() {
     rectangle.appendChild(créerBouton());
 }
 
-// Call the function to construct the interface
 constuireInterfaceQuestion();
 rectangleDesDonnées.appendChild(rectangle);
-
-const rectangleDesDonnees = document.getElementById("zoneDeDonnees");
-
-
-const rectangle = document.createElement("div");
-rectangle.id = "rectangle";
-
-// ici on fait la magnifique création du titre
-const titre = document.createElement("div");
-titre.id = "titre";
-titre.textContent = "Salam maleykoum, bienvenue sur le quiz JavaScript de Janelle et Raphael مرحبًا سيد جوليه، إليك اختبارنا المقدس";
-
-// ici on fait la création des paragraphes
-
-const paragraphe1 = document.createElement("p1");
-paragraphe1.textContent = "JEU QUESTIONNAIRE :o";
-const paragraphe2 = document.createElement("p2");
-paragraphe2.textContent = "Je vous invite à participer à un petit jeu questionnaire qui comporte 5 questions choisies au hasard dans un ensemble de questions. " +
-    "Pour chaque question, vous devez choisir une réponse parmi les 4 choix proposés. À la fin du questionnaire, vous aurez votre résultat. ";
-
-
-// ici on fait la création du bouton
-const bouton = document.createElement("bouton");
-bouton.id = "bouton";
-bouton.textContent = "Commencer le quiz";
-bouton.addEventListener("click", function () {
-    alert("Vous avez cliqué sur le bouton")
-    viderLinterieurDuRectangle();
-});
-
-/**
- * Fonction qui vide le rectangle après le click du bouton
- */
-function viderLinterieurDuRectangle() {
-    while (rectangle.firstChild) {
-        rectangle.removeChild(rectangle.firstChild);
-    }
-}
-
-// on fait les appends, ca ressemble a ce que le prof a fait
-rectangle.appendChild(titre);
-rectangle.appendChild(paragraphe1);
-rectangle.appendChild(paragraphe2);
-rectangle.appendChild(bouton);
-
-// fait le append du rectangle dans le rectangle des données
-rectangleDesDonnees.appendChild(rectangle);
 
 
 
