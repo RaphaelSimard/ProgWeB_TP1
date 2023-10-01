eeb1f4ad7ac4b6567315868a02c2cc0de06be036
JanelleB2 <2264502@cegeplimoilou.ca>
2023-10-01 15:54:40 -0400

    structuration du questionnaireJS
    juste des commentaires et documentation pour s'aligner (stats github chuuuut)

--- RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
+++ RaphaelSimard_JanelleBédard – Tp 1-Questionnaire – A23/js/tp1-Questionnaire.js
@@ -1,21 +1,25 @@
 "use strict"
 
-//Mettre votre code JS ici
-
+//Créations des variables et constantes et d'autres trucs randoms
 const rectangleDesDonnees = document.getElementById("zoneDeDonnees");
 const rectangle = document.createElement("div");
 rectangle.id = "rectangle";
 
 
+/**
+ * Function qui démarre le jeu lorsque l'utilisateur clique sur le bouton "Commencer le quiz"
+ * @returns {*} un bouton ? pk faut-il le retourner ?
+ */
 function creerBouton() {
     let bouton = creerBaliseX("button", "bouton", "Commencer le quiz", "");
     bouton.addEventListener("click", function () {
-        //Ici on va appeller la fonction qui crée l'interface de la question
-        construireInterfaceQuestion();
-    });
+        construireInterfaceQuestion();});
     return bouton;
 }
 
+/**
+ * Fonction qui crée l'interdface d'introduction du jeu.
+ */
 function constuireInterfaceIntro() {
     rectangle.appendChild(creerBaliseX("h1", "titre", "Bienvenue sur le quiz JavaScript de Janelle et Raphael"));
     rectangle.appendChild(creerBaliseX("p", "p1", "JEU QUESTIONNAIRE :o"));
@@ -23,23 +27,26 @@ function constuireInterfaceIntro() {
     rectangle.appendChild(creerBouton());
 }
 
-
-var i = 0;
-var correctCount = 0;
-
+/**
+ * Fonction qui s'occupe seulement de faire un tableau ordonné et d'y mettre les questions récupérées du JSON.
+ */
 function creationTableauQuestions() {
+    let tableauDesQuestions;
     for (const question of tabAssQuestions) {
-        console.log(question); // Use 'question' to access each question in the array
+        question.push(tableauDesQuestions);
     }
-//ON PEUT ENFIN ACCEDER AUX QUESTIONS!!!! LETS GOOOOOOOOOOOOOOO MASHALLAH
+    //Hop hop ici on a du boulot
 
 }
 
-
+/**
+ * Fonction qui s'occupe de créer l'interface des questions, les boutons, la question, les réponses, etc.
+ */
 function construireInterfaceQuestion() {
     //vider le cadre de travail
     rectangle.innerHTML = "";
 
+    //Ici il faut changer le titre pour le numéro de la question (index +1)
     rectangle.appendChild(creerBaliseX("h1", "titre", "Question 1"));
     creationTableauQuestions();
 
@@ -49,12 +56,14 @@ function construireInterfaceQuestion() {
     rectangle.appendChild(creerInput("button", "bouton", "", "Passez à la question suivante", ""));
     rectangle.appendChild(creerInput("button", "bouton", "", "Abandonner la question", ""));
     //afficher la premiere question
+    
     //bruh ici c'est une zone de texte ??
     rectangle.appendChild(creerInput("p", "p1", tabAssQuestions[0].question1, "", ""));
 
 
 }
 
+// Appel des fonctions:
 constuireInterfaceIntro(rectangleDesDonnees.appendChild(rectangle));
 
 
